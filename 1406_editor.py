235f53bf6b50fad28079263a952f738eea272cf5
hyukkyo <shk5644@gmail.com>
2022-12-05 02:35:51 +0900

    timeout fail

--- 1406_editor.py
+++ 1406_editor.py
@@ -1,26 +1,49 @@
-sentence = input()
-m = int(input())
+# s = input()
+# m = int(input())
+# n = len(s)
+# i=0
+# while i < m:
+#     c = input()
+#     if c == 'L':
+#         n -= 1
+#     elif c == 'D':
+#         if len(s) > n:
+#             n += 1
 
-cursor = len(sentence)
-
-for i in range(m):
-    command = input()
+#     elif c == 'B':
+#             s = s[:n-1] + s[n:]
+#     else:
+#         s = s[:n] + c[2] + s[n:]
+#         n += 1
     
-    if command == 'L':
-        if cursor > 0:
-            cursor -= 1
-    elif command == 'D':
-        if len(sentence) > cursor:
-            cursor += 1
+#     if n < 0:
+#         n = 0    
+#     i += 1
+# print(s)
+#timeout
 
-    elif command == 'B':
-        if cursor > 0:
-            sentence = sentence[:cursor-1] + sentence[cursor:]
-            cursor -= 1
+#스택 두개로 나눠서 커서를 다루는 풀이가 있었음.
+import sys
+
+st1 = list(sys.stdin.readline().rstrip())
+st2 = []
+
+for _ in range(int(sys.stdin.readline())):
+    command = list(sys.stdin.readline().split())
+    if command[0] == 'L':
+        if st1:
+            st2.append(st1.pop())
+            
+    elif command[0] == 'D':
+        if st2:
+            st1.append(st2.pop())
+
+    elif command[0] == 'B':
+        if st1:
+            st1.pop()
+            
     else:
-        sentence = sentence[:cursor] + command.split(' ')[1] + sentence[cursor:]
-        cursor += 1
-
-print(sentence)
-
-# timeout
+        st1.append(command[1])
+        
+st1.extend(reversed(st2))
+print(''.join(st1))
