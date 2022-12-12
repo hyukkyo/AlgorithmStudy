3ce5b2b0f673e1b5284b5a6946994a16a6bb3c45
hyukkyo <shk5644@gmail.com>
2022-12-12 16:02:26 +0900

    success

--- 17413_reverse_word_2.py
+++ 17413_reverse_word_2.py
@@ -1,3 +1,38 @@
-import sys
+S = input()
 
-S = list(sys.stdin.readline().split())
+spell = []
+word = []
+
+tag = 0
+for i in S:
+    if i == '<':
+        if(len(spell) != 0):
+            spell.reverse()
+            word.append(''.join(spell))
+            spell.clear()
+        tag = 1
+
+    if tag == 0:
+        if i != ' ':
+            spell.append(i)
+        else:
+            if(len(spell) != 0):
+                spell.reverse()
+                spell.append(' ')
+                word.append(''.join(spell))
+                spell.clear()
+    elif tag == 1:
+        spell.append(i)
+
+    if i == '>':
+        if(len(spell) != 0):
+            word.append(''.join(spell))
+            spell.clear()
+        tag = 0
+
+if(len(spell) != 0):
+    spell.reverse()
+    word.append(''.join(spell))
+    spell.clear()
+
+print(''.join(word))
