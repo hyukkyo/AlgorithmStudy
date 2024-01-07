73c700b33b14d81e6402c619c4864e8a98814d3f
hyukkyo <shk5644@gmail.com>
2024-01-07 19:41:45 +0900

    반복문

--- LeetCode/236_Lowest_Common_Ancestor.py
+++ LeetCode/236_Lowest_Common_Ancestor.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
@@ -13,4 +13,10 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
+        # LCA는 최소 공통 조상을 말함
+        # LCA(p, q) = 자식노드 p와 q의 가장 가까운 부모 노드
         
+
+
+print(Solution.lowestCommonAncestor(root = [3,5,1,6,2,0,8,None,None,7,4], p = 5, q = 1))
+print(Solution.lowestCommonAncestor(root = [3,5,1,6,2,0,8,None,None,7,4], p = 5, q = 4))
