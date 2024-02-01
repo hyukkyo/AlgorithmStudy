e57353dc35fb6858622efc206b8b3bb6c8d7e262
hyukkyo <shk5644@gmail.com>
2024-02-01 19:40:48 +0900

    bfs

--- LeetCode/104_Max_Depth.py
+++ LeetCode/104_Max_Depth.py
@@ -10,7 +10,7 @@ class TreeNode(object):
 # level order (BFS)로 풀어보자
 class Solution(object):
     def maxDepth(self, root):
-        depth = 0
+        max_depth = 0
 
         visited = []
         q = deque()
@@ -18,15 +18,16 @@ class Solution(object):
         if root == None:
             return 0
         
-        q.append(root)
+        q.append((root, 1))
 
         while q:
-            current_node = q.popleft()
+            current_node, current_depth = q.popleft()
             visited.append = current_node.val
+            max_depth = max(current_depth, max_depth)
 
             if current_node.left:
-                q.append(current_node.left)
+                q.append((current_node.left, current_depth+1))
             if current_node.right:
-                q.append(current_node.right)
+                q.append((current_node.right, current_depth+1))
 
-        return depth
+        return max_depth
