2f4d85633ea5bad90e9efd82b9a11b4612169f30
hyukkyo <shk5644@gmail.com>
2024-02-03 21:41:10 +0900

    graph dfs

--- Graph_DFS.py
+++ Graph_DFS.py
@@ -1,6 +1,3 @@
-
-
-
 graph = {
     'A':['B','C','D'],
     'B':['A','C','D'],
@@ -9,4 +6,13 @@ graph = {
     'E':['A'],
 }
 
-dfs(graph, 'A')
+# 매개변수가 많아서 visited는 걍 밖으로 빼줌
+visited = []
+
+def dfs(cur_v):
+    visited.append(cur_v)
+    for v in graph[cur_v]:
+        if v not in visited:
+            dfs(v)
+
+dfs('A')
