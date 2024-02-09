04d8712f25a2bde5eb2e34e61413d30a15f16da4
hyukkyo <shk5644@gmail.com>
2024-02-09 19:31:37 +0900

    그래프 dfs 적용

--- LeetCode/841-keys-and-rooms.py
+++ LeetCode/841-keys-and-rooms.py
@@ -1,8 +1,22 @@
-# DFS
+# DFS, BFS 둘다 할 수 있음.
+
+# DFS의 시간복잡도는 O(V+E) 완전탐색
+
 
 def canVisitAllRooms(rooms):
-    pass
+    visited = [False] * len(rooms)
+    visited[0] = True
+
+    # cur_v에 연결된 모든 vertex에 방문한다.
+    def dfs(cur_v):
+        visited[cur_v] = True
+        for next_v in rooms[cur_v]:
+            if visited[next_v] == False:
+                dfs(next_v)
+
+    dfs(0)
+
+    return all(visited)
 
 
-
-print(canVisitAllRooms());
+print(canVisitAllRooms([[1],[2],[3],[]]))
