4ba2f70e5bbc8e9eb2d97056e6be0e283c4c0cef
hyukkyo <shk5644@gmail.com>
2024-02-24 04:23:09 +0900

    bfs

--- Baekjoon/2178-maze-search.py
+++ Baekjoon/2178-maze-search.py
@@ -7,13 +7,12 @@ grid = []
 for _ in range(N):
     grid.append(list(map(int, sys.stdin.readline().strip())))
 
-path = grid
+path = grid # 입력값 grid는 두고 path를 조정할 예정
 
-def bfs(grid, x, y):
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-
+def bfs(x, y):
     path[x][y] = 1
     queue = deque()
     queue.append((x, y))
@@ -24,11 +23,10 @@ def bfs(grid, x, y):
             next_y = cur_y + dy[i]
             if next_x >= 0 and next_x < N and next_y >= 0 and next_y < M:
                 if grid[next_x][next_y] == 1 and path[next_x][next_y] == 1:
-                    path[next_x][next_y] += 1
+                    path[next_x][next_y] = path[cur_x][cur_y] + 1
                     queue.append((next_x, next_y))
 
     return path[-1][-1]
 
-
-
-print(bfs(grid, 0, 0))
+print(bfs(0, 0))
+# print(path)
