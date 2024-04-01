553aeafcbcaac7aec7c409019c4b46b4f40237fa
hyukkyo <shk5644@gmail.com>
2024-04-01 09:57:49 +0900

    그냥 구현

--- Baekjoon/10709_weather_caster.py
+++ Baekjoon/10709_weather_caster.py
@@ -4,12 +4,25 @@ input = sys.stdin.readline
 h, w = map(int, input().split())
 
 grid = []
-for i in range(h):
-    line = input()
-    w = []
-    for j in range(w):
-        w.append(line[j])
-        
-    
+result = [[-1]*w for _ in range(h)]
 
-print(grid)
+
+for i in range(h):
+    line = list(input().strip())
+    grid.append(line)
+    for j in range(w):
+
+        if line[j] == 'c':
+            result[i][j] = 0
+
+
+for i in range(h):
+    for j in range(1, w):
+        if result[i][j] == -1 and result[i][j-1] != -1:
+            result[i][j] = result[i][j-1] + 1
+
+
+for i in range(h):
+    for j in range(w):
+        print(result[i][j], end=" ")
+    print()
