f58c9c5b40cce15f602e6422d2a8ff864740181d
hyukkyo <shk5644@gmail.com>
2024-04-06 19:35:58 +0900

    bfs

--- Baekjoon/1012_organic_cabbage.py
+++ Baekjoon/1012_organic_cabbage.py
@@ -6,59 +6,44 @@ input = sys.stdin.readline
 t = int(input())
 result = []
 
-# def bfs(x, y, visited):
-#     dx = [1, -1, 0, 0]
-#     dy = [0, 0, 1, -1]
-
-#     visited[y][x] = True
-#     queue = deque()
-#     queue.append((x, y))
-
-#     while queue:
-#         cur_x, cur_y = queue.popleft()
-#         for i in range(4):
-#             next_x = cur_x + dx[i]
-#             next_y = cur_y + dy[i]
-#             if (0 <= next_x < m) and (0 <= next_y < n):
-#                 if grid[next_y][next_x] == 1 and not visited[next_y][next_x]:
-#                     visited[next_y][next_x] = True
-#                     queue.append((next_x, next_y))
-
 for i in range(t):
     m, n, k = map(int, input().split())
-    grid = [[0]*m for _ in range(n)]
+    grid = [[0]*n for _ in range(m)]
 
     for j in range(k):
-        x, y = map(int, input().split())
+        y, x = map(int, input().split())
         grid[y][x] = 1
     # 세팅 완료
 
 
-    visited = [[False]*m for _ in range(n)]
+    visited = [[False]*n for _ in range(m)]
     
-    def bfs(x, y):
+    
+    def bfs(y, x):
         dx = [1, -1, 0, 0]
         dy = [0, 0, 1, -1]
 
         visited[y][x] = True
         queue = deque()
-        queue.append((x, y))
+        queue.append((y, x))
 
         while queue:
-            cur_x, cur_y = queue.popleft()
+            cur_y, cur_x = queue.popleft()
             for i in range(4):
                 next_x = cur_x + dx[i]
                 next_y = cur_y + dy[i]
-                if (0 <= next_x < m) and (0 <= next_y < n):
+                if (0 <= next_x < n) and (0 <= next_y < m):
                     if grid[next_y][next_x] == 1 and not visited[next_y][next_x]:
                         visited[next_y][next_x] = True
-                        queue.append((next_x, next_y))
+                        queue.append((next_y, next_x))
+
+
                         
     num = 0
-    for j in range(n):
-        for k in range(m):
+    for j in range(m):
+        for k in range(n):
             if grid[j][k] == 1 and visited[j][k] == False:
-                bfs(j, k, visited=visited)
+                bfs(j, k)
                 num += 1
 
     print(num)
